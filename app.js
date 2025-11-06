48e8ec7b7defd5e9da9eaeabb6d01e8fa9ed18a2
Mika <76068667+Triplecalc@users.noreply.github.com>
2025-11-06 18:31:47 +0500

    Update app.js
    метрика

--- app.js
+++ app.js
@@ -537,6 +537,21 @@ document.addEventListener('DOMContentLoaded', function() {
     copyToClipboard('повторное обращение');
   });
   
+//Метрика
+  <!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105145824', 'ym');
+
+    ym(105145824, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/105145824" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
   // Агент не связался
   qs('#agent-not-contacted').addEventListener('click', () => {
     copyToClipboard('агент не связался с клиентом, просьба связаться');
